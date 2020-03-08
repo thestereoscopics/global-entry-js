4ef1bd41c2222390e4109f8b589f4de0c23a4af5
Jeremy Sell <jeremy@teamroboboogie.com>
2020-03-07 19:10:29 -0800

    add favicon, change the way colors work

--- index.js
+++ index.js
@@ -1,44 +1,66 @@
-$(document).ready(function () {
+var settings = {
+    "airport_id": 7960,
+    "halifax_id_for_testing": 5031,
+    "notify_me_if_appt_is_before_date": "May 14, 2020",
+    "number_of_appts_to_show": 6,
+}
 
-    var settings = {
-        "airport_id": 7960,
-        "halifax_id_for_testing": 5031,
-        "notify_me_if_appt_is_before_date": "May 14, 2020",
-        "number_of_appts_to_show": 6,
+let timeleft = 9;
+const main = document.querySelector('.main');
+const body = document.querySelector('body');
+
+getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
+};
 
-    function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
+
+
+secondsToRefresh = () => {
+    const refreshTimer = setInterval(function () {
+        if (timeleft === 1) {
+            document.querySelector(".counter").innerHTML = timeleft;
+            clearInterval(refreshTimer);
+            timeleft = 10;
+        } else {
+            document.querySelector(".counter").innerHTML = timeleft;
+            timeleft -= 1;
         }
-        return color;
-    }
+        
+    }, 1000);
+};
 
-    checkForAppt = () => {
-        $.ajax({
-            url: `https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=${settings.number_of_appts_to_show}&locationId=${settings.airport_id}&minimum=1`
-        }).done(function(data) {
+checkForAppt = () => {
+    secondsToRefresh();
+    fetch(`https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=${settings.number_of_appts_to_show}&locationId=${settings.airport_id}&minimum=1`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
             if (data.length) {
-                var appts = '';
+                let appts = '';
                 for (let i = 0; i < data.length; i++) {
-                    var date = new Date(data[i].startTimestamp).toDateString();
-                    var hours = `${data[i].startTimestamp.split('T')[1]} - ${data[i].endTimestamp.split('T')[1]}`;
+                    const date = new Date(data[i].startTimestamp).toDateString();
+                    const hours = `${data[i].startTimestamp.split('T')[1]} - ${data[i].endTimestamp.split('T')[1]}`;
                     appts += `<span><a target="_blank" href="https://ttp.cbp.dhs.gov/">${date} ${hours}</a></span>`;
                 }
-                $('.main').html(appts).css('color', getRandomColor());
-                
+
+                main.innerHTML = appts;
+
                 if (new Date(data[0].startTimestamp) < new Date(settings.notify_me_if_appt_is_before_date)) {
-                    var audio = new Audio('global-entry-here-we-come.wav');
+                    const audio = new Audio('global-entry-here-we-come.wav');
                     audio.play();
                 }
             } else {
-                $('.main').html(`<span><a target="_blank" href="https://ttp.cbp.dhs.gov/">Still Checking at ${new Date()}</a></span>`).css('color', getRandomColor());
+                main.innerHTML = `<span><a target="_blank" href="https://ttp.cbp.dhs.gov/">Still Checking at ${new Date()}</a></span>`;
             }
+            body.style["background-color"] = getRandomColor();
         });
-    }
+};
 
-    setInterval(checkForAppt, 10000);
-    checkForAppt();
-});
+setInterval(checkForAppt, 10000);
+checkForAppt();
