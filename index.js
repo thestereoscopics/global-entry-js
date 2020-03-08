76e1eba65817732952d0b4a56ab0f8f04b4ea100
Jeremy Sell <jeremy@teamroboboogie.com>
2020-03-07 18:28:15 -0800

    better ui, more meaningful dates, and link to tsa site

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
-    var halifaxURL = 'https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=3&locationId=5031&minimum=1';
-    var pdxURL = 'https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=3&locationId=7960&minimum=1';
+
+    var settings = {
+        "airport_id": 7960,
+        "halifax_id_for_testing": 5031,
+        "notify_me_if_appt_is_before_date": "May 14, 2020",
+        "number_of_appts_to_show": 6,
+    }
 
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
@@ -13,25 +18,27 @@ $(document).ready(function () {
 
     checkForAppt = () => {
         $.ajax({
-            url: pdxURL
+            url: `https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=${settings.number_of_appts_to_show}&locationId=${settings.airport_id}&minimum=1`
         }).done(function(data) {
             if (data.length) {
                 var appts = '';
                 for (let i = 0; i < data.length; i++) {
-                    appts += '<span>' + new Date(data[i].startTimestamp).toDateString() + '</span>';
+                    var date = new Date(data[i].startTimestamp).toDateString();
+                    var hours = `${data[i].startTimestamp.split('T')[1]} - ${data[i].endTimestamp.split('T')[1]}`;
+                    appts += `<span><a target="_blank" href="https://ttp.cbp.dhs.gov/">${date} ${hours}</a></span>`;
                 }
                 $('.main').html(appts).css('color', getRandomColor());
-
-                if (new Date(data[0].startTimestamp) < new Date('August 01 2020')) {
+                
+                if (new Date(data[0].startTimestamp) < new Date(settings.notify_me_if_appt_is_before_date)) {
                     var audio = new Audio('global-entry-here-we-come.wav');
-                    //audio.play();
+                    audio.play();
                 }
             } else {
-                $('.main').html('<span>Nothing</span>').css('color', getRandomColor());
+                $('.main').html(`<span><a target="_blank" href="https://ttp.cbp.dhs.gov/">Still Checking at ${new Date()}</a></span>`).css('color', getRandomColor());
             }
         });
     }
 
-    setInterval(checkForAppt, 5000);
+    setInterval(checkForAppt, 10000);
     checkForAppt();
 });
