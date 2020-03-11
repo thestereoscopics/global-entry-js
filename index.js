a1c78b86220cb3d9ec29637ad319a99b2d44d25d
Jeremy Sell <jeremy@teamroboboogie.com>
2020-03-10 22:50:39 -0700

    updating for github pages

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var settings = {
-    "airport_id": 7960,
+    "locationID": 7960,
     "halifax_id_for_testing": 5031,
     "notify_me_if_appt_is_before_date": "May 14, 2020",
     "number_of_appts_to_show": 6,
+    "ecAddress": "7000 PDX AIRPORT Room T3352 Portland OR 97218 US",
 }
 
 let timeleft = 9;
+let intervalForAppt, refreshTimer;
 const main = document.querySelector('.main');
 const body = document.querySelector('body');
+const addressEl = document.querySelector('.address');
 
 getRandomColor = () => {
     const letters = '0123456789ABCDEF';
@@ -18,10 +21,8 @@ getRandomColor = () => {
     return color;
 };
 
-
-
 secondsToRefresh = () => {
-    const refreshTimer = setInterval(function () {
+    refreshTimer = setInterval(function () {
         if (timeleft === 1) {
             document.querySelector(".counter").innerHTML = timeleft;
             clearInterval(refreshTimer);
@@ -35,19 +36,20 @@ secondsToRefresh = () => {
 };
 
 checkForAppt = () => {
-    secondsToRefresh();
-    fetch(`https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=${settings.number_of_appts_to_show}&locationId=${settings.airport_id}&minimum=1`)
+    // secondsToRefresh();
+    body.style["background-color"] = getRandomColor();
+    fetch(`https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=${settings.number_of_appts_to_show}&locationId=${settings.locationID}&minimum=1`)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
             if (data.length) {
                 let appts = '';
-                for (let i = 0; i < data.length; i++) {
-                    const date = new Date(data[i].startTimestamp).toDateString();
-                    const hours = `${data[i].startTimestamp.split('T')[1]} - ${data[i].endTimestamp.split('T')[1]}`;
+                data.map((eachDate) => {
+                    const date = new Date(eachDate.startTimestamp).toDateString();
+                    const hours = `${eachDate.startTimestamp.split('T')[1]} - ${eachDate.endTimestamp.split('T')[1]}`;
                     appts += `<span><a target="_blank" href="https://ttp.cbp.dhs.gov/">${date} ${hours}</a></span>`;
-                }
+                })
 
                 main.innerHTML = appts;
 
@@ -56,11 +58,36 @@ checkForAppt = () => {
                     audio.play();
                 }
             } else {
-                main.innerHTML = `<span><a target="_blank" href="https://ttp.cbp.dhs.gov/">Still Checking at ${new Date()}</a></span>`;
+                main.innerHTML = `<span><a target="_blank" href="https://ttp.cbp.dhs.gov/">There are zero appointments right now</a></span>`;
             }
-            body.style["background-color"] = getRandomColor();
         });
 };
 
-setInterval(checkForAppt, 10000);
-checkForAppt();
+startCheck = () => {
+    intervalForAppt = setInterval(checkForAppt, 10000);
+    checkForAppt();
+}
+
+const dateBox = document.querySelector('#last-day');
+const enrollmentCenters = document.querySelector('#enrollment-center');
+
+getEnrollmentID = (obj) => {
+    settings.ecAddress = obj.options[obj.selectedIndex].dataset.address;
+    settings.locationID = enrollmentCenters.value;
+    addressEl.innerHTML = `<p>${settings.ecAddress}</p>`;
+    clearInterval(intervalForAppt);
+    // clearInterval(refreshTimer);
+    startCheck();
+}
+
+enrollmentCenters.addEventListener('change', function() {getEnrollmentID(this)});
+dateBox.addEventListener('focusout', (event) => {
+    if (event.target.value) {
+        settings.notify_me_if_appt_is_before_date = event.target.value;
+        clearInterval(intervalForAppt);
+        // clearInterval(refreshTimer);
+        startCheck();
+    }
+});
+
+startCheck();
